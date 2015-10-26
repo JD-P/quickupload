0d132aaa2146f8bcb32a5fc313b4b93dac4a31fb
John David Pressman <jdpostage@linuxmail.org>
2015-10-26 00:08:48 -0700

    Test, trying to see what happens if I don't send an OK response.

--- cgi-bin/upload.py
+++ cgi-bin/upload.py
@@ -15,7 +15,7 @@ def upload_file():
     with open("uploads/" + filename) as outfile:
         outfile.write(file_contents)
         outfile.flush()
-    send_OK()
+    #send_OK()
 
 def sanitize_filename(filename):
     """Sanitize a filename, removing dangerous characters that could be used to 
